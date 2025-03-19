7bd4b70680ee472759d80abde03c21d8d33719ab
Michael-Angelo Karpowicz <michaelangelo.karpowicz@gmail.com>
2025-03-20 00:38:51 +0100

    Add python code for loops.py

--- fundamentals/loops.py
+++ fundamentals/loops.py
@@ -5,7 +5,21 @@ import random
 # 4 Keeps track of the number of guesses and displays it when the user guesses correctly
 random.seed()
 correctNumber = random.randrange(1, 100)
+lowest = 1
+highest = 100
+guess = 0
+counter = 0
 
-do 
-  guess = input("Guess a number between 1-100: ")
-while (guess !=)
+while True:
+  guess = int(input("Guess a number between 1-100: "))
+  counter += 1
+  if guess < lowest or guess > highest:
+    print(f"Please enter a number between {lowest} and {highest}.")
+    continue
+  if guess < correctNumber:
+    print("Too low!")
+  elif guess > correctNumber:
+    print("Too high!")
+  else:
+    print(f"Correct! You guessed the number in {counter} tries.")
+    break
