90dcb779e546fbe0d3e7869553769af00a424367
Michael-Angelo Karpowicz <michaelangelo.karpowicz@gmail.com>
2025-03-25 20:34:52 +0100

    Add code to gradebook, but its not finished

--- fundamentals/gradebook.py
+++ fundamentals/gradebook.py
@@ -1,9 +1,27 @@
-gradebook = {
-  "Alice": {
-    "math_grade": 1
-  }, 
-  "Bob": {
-    "math_grade": 92
-  }
-}
+gradebook = {"Alice": 85, "Bob": 9}
 
+while True:
+  print("\nGradebook Manager: ")
+  print("1. Add")
+  print("2. Update")
+  print("3. Calculate average")
+  print("4. Print top-scoring student")
+  print("5. Exit")
+  
+  choice = input("Choose an option: ")
+  if choice == "1" or choice == "Add":
+    student = input("Name the student you want to add: ")
+    if student in gradebook
+    
+  if choice == "2":
+    print("2")
+    
+  if choice == "3":
+    print("3")
+    
+  if choice == "4":
+    print("4")
+    
+  if choice == "5":
+    print("Exiting...")
+    break
