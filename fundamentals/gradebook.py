ed1d8327331429feb2832ebde6803ec1d8fb1de4
Michael-Angelo Karpowicz <michaelangelo.karpowicz@gmail.com>
2025-03-25 21:05:17 +0100

    Add code to gradebook

--- fundamentals/gradebook.py
+++ fundamentals/gradebook.py
@@ -6,13 +6,18 @@ while True:
   print("2. Update")
   print("3. Calculate average")
   print("4. Print top-scoring student")
-  print("5. Exit")
+  print("5. View all students")
+  print("6. Exit")
   
   choice = input("Choose an option: ")
   if choice == "1" or choice == "Add":
     student = input("Name the student you want to add: ")
-    if student in gradebook
-    
+    if student not in gradebook:
+      grade = input("Give him/her a grade: ")
+      gradebook[student] = grade
+    else:
+      print(f"{student} is already listed in the gradebook")
+          
   if choice == "2":
     print("2")
     
@@ -23,5 +28,10 @@ while True:
     print("4")
     
   if choice == "5":
+    for n in gradebook:
+      print(f"- {n}: {gradebook.get(n)}")
+      
+    
+  if choice == "6":
     print("Exiting...")
     break
