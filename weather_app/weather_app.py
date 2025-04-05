6779c6476aa17a86d9602ea5fff5b5a6ef1b81b5
Michael-Angelo Karpowicz <michaelangelo.karpowicz@gmail.com>
2025-04-05 23:02:22 +0200

    Add delete history

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -1,33 +1,44 @@
 # [x] Display temperature, humidity, and weather conditions for a user-input city
-# [ ] Save each successful weather query to a file
-# [ ] Display search history when the user types "history"
-# [ ] Bonus: Add timestamps to each entry
+# [x] Save each successful weather query to a file
+# [x] Display search history when the user types "history"
+# [x] Add timestamps to each entry
 
+import os
 from config import API_KEY
 from requests import get
+from datetime import datetime
 
 def displayWeather(jsondata):
     print(f"Weather data in {city.title()}")
     print(f"Temperature is: {jsondata['main']['temp'] - 272.15:.1f}°C")
     print(f"Humidity is: {jsondata['main']['humidity']}")
-    print(f"Weather condition is: {jsondata['weather'][0]['description']}")
+    print(f"Weather condition is:")
 
+# Get the script's directory and set history file path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+history_file = os.path.join(script_dir, "weather_history.txt")
 
+print("=== Weather app ===")
 while True:
     # Get user input for city
     try:
-        print("=== Weather app ===")
-        city = input("What city would you like to know it's weather conditions of? ").strip().lower()
+        city = input("\nWhat city would you like to know it's weather conditions of? ").strip().lower()
         if not city:
             print("Format does not allow empty input")
             continue
         elif city == "history":
             try:
-                with open("weather_history.txt", "r") as file:
+                with open(history_file, "r") as file:
                     print("Search history:")
                     print(file.read())
             except FileNotFoundError:
-                print("File not found")
+                print("File not found!")
+            continue
+        elif city == "delete history":
+            if os.path.exists(history_file):
+                os.remove(history_file)
+            else:
+                print("File not found!")  
             continue
         elif city == "exit":
             print("Exiting... Goodbye!")
@@ -45,8 +56,9 @@ while True:
                 jsonResponse = response.json()
                 
                 # Save succesfull query in a file
-                with open("weather_history.txt", "a") as file:
-                    file.write(f"city: {city.title()}, temperature: {jsonResponse['main']['temp'] - 272.15:.1f}°C\n")
+                with open(history_file, "a") as file:
+                    now = datetime.now().strftime("%d.%m.%Y %H-%M")
+                    file.write(f"\ndate: {now} city: {city.title()}, temperature: {jsonResponse['main']['temp'] - 272.15:.1f}°C")
                 
                 # print(json.dumps(jsonResponse, indent=4))
                 
