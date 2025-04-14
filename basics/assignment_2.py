8a87e162b565cd23c606999dc715e355d90f8aca
Michael-Angelo Karpowicz <michaelangelo.karpowicz@gmail.com>
2025-04-14 17:23:23 +0200

    Finish assignment_3.py

--- basics/assignment_2.py
+++ basics/assignment_2.py
@@ -1,6 +1,3 @@
-# You are a Marvel fan and created a list of superheroes.
-# avengers  = [‘Iron Man’, ‘Captain America’, ‘Black Widow’, ‘Hulk’, ‘Thor’, ‘Hawkeye’]
-# Using this list, calculate how many members in the Avengers team?
 
 avengers  = ['Iron Man', 'Captain America', 'Black Widow', 'Hulk', 'Thor', 'Hawkeye']
 
@@ -9,34 +6,21 @@ for avenger in avengers:
     total_avengers += 1
 print(f"There are {total_avengers} members in the Avengers team")
 
-# Now Iron Man made Spider-Man a new member of the Avengers, add him to your list at the end
 
 avengers.append("Spider-Man")
 # print(avengers)
 
 
-# Let's add a twist to it. Everyone came to a conclusion that Captain America is the leader of the Avengers, so please add him before the Iron Man.
-# Hint: You can first remove him from the list and add before the Iron Man. To remove him you can use a method called .pop()
 avengers.remove("Captain America")
 avengers.insert(0, "Captain America")
 # print(avengers)
 
 
 
-# Thor and Hulk are getting angry easily and fight with each other. So you have to separate them with each other.
-# To separate them, move “Black Widow” in between them.
-
 avengers.remove("Black Widow")
 avengers.insert(3,"Black Widow")
 # print(avengers)
 
-
-# Below list contains scores of students in a class as per their roll number. Which means the first element is for roll # 1, second one is for roll # 2 and so on. Print the scores of,
-#
-# 1 The first student
-# 2 The last student
-# 3 First 3 students
-# 4 Scores of roll # 3, 4 and 5
 
 scores = [92, 85, 76, 58, 89, 91, 73, 84]
 
@@ -56,23 +40,6 @@ print(f"First student: {scores[2:5]}")
 scores.append(83)
 print(scores)
 
-# Categorizes each score into a grade based on the following thresholds:
-#
-# 1 A: 90 to 100
-# 2 B: 80 to 89
-# 3 C: 70 to 79
-# 4 D: 60 to 69
-# 5 F: Below 60
-# Count the number of students in each grade category and print the summary of how many students received each grade.
-#
-# Expected output
-#
-# Grade Summary:
-# - A: 2 students
-# - B: 4 students
-# - C: 2 students
-# - D: 0 students
-# - F: 1 students
 
 a_count = b_count = c_count = d_count = f_count = 0
 for score in scores:
@@ -94,17 +61,6 @@ print(f"- C: {c_count} students")
 print(f"- D: {d_count} students")
 print(f"- F: {f_count} students")
 
-# Managing inventory efficiently is crucial for businesses to ensure they do not run out of key products. This exercise simulates a simple inventory management system where the user can see which items are below the minimum stock level and need reordering. Below you have two lists that stores product names and their inventory stock levels.Using that,
-#
-# 1 Check each item to see if its stock level is below a minimum threshold.
-# 2 If the stock level is below the minimum, add the product's name to a reorder list.
-# 3 Print a list of products that need to be reordered.
-#
-# Expected Output
-#
-# Items to Reorder:
-# - Pears
-# - Grapes
 
 product_names = ["Apples", "Bananas", "Oranges", "Pears", "Grapes"]
 stock_levels = [20, 50, 15, 5, 8]
