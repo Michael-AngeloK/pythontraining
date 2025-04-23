72e9419614fdd12deeb153f656e2a0ed45a1eb19
Michael-Angelo Karpowicz <michaelangelo.karpowicz@gmail.com>
2025-04-23 19:03:20 +0200

    Restructure file directories

--- lambda/mathoperations.py
+++
@@ -1,8 +0,0 @@
-square = lambda a: a ** 2
-
-try:
-    input = int(input("Enter a number to be squared: "))
-    print("The squared number is:", square(input))
-except ValueError as a:
-    print(f"This input is invalid: {a}")
-    
--- lambda/sort_students.py
+++
@@ -1,5 +0,0 @@
-gradebook = [("Alice", 88), ("Bob", 92), ("Charlie", 75)]
-
-sorted_gradebook = sorted(gradebook, key=lambda a: a[1], reverse=True)
-
-print(sorted_gradebook)
